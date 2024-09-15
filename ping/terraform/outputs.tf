e657a03528a967087204053dd21a9146a0dbeb84
alvarioto <aldipo7292@gmail.com>
2024-09-15 20:56:07 +0200

    Delete ping directory

--- ping/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "source_registry" {
-  description = "El nombre del registro de contenedores de origen"
-  value       = azurerm_container_registry.source_registry.name
-}
-
-output "destination_registry" {
-  description = "El nombre del registro de contenedores de destino"
-  value       = azurerm_container_registry.destination_registry.name
-}
